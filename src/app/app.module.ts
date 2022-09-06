41e390deb73cf3d8094972c0460490bcc635cd78
Aidan Beckman <beckmanap@vcu.edu>
2022-09-06 13:58:06 -0400

    added todoItems

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TodoItemsComponent } from './todo-items/todo-items.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoItemsComponent
   ],
   imports: [
     BrowserModule
