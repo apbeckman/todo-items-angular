945f32500ed4eb9fcc5765553eb6353985dab0f2
Aidan Beckman <aidan.beckman@gmail.com>
2022-09-14 18:48:06 -0400

    added todo interface

--- src/app/todo-items/todo-items.component.ts
+++ src/app/todo-items/todo-items.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from '../todo';
 
 @Component({
   selector: 'app-todo-items',
@@ -6,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-items.component.css']
 })
 export class TodoItemsComponent implements OnInit {
+  todo: Todo = {
+    priority: 1,
+    name: 'Laundry'
+  }
 
   constructor() { }
-
   ngOnInit(): void {
   }
 
---
+++ src/app/todo.ts
@@ -0,0 +1,5 @@
+export interface Todo {
+  priority: number;
+  name: string;
+
+}
